d29a66fda6e545157ff39e39488ca137ba0d8f96
Jeff Olson <jolson615@gmail.com>
2022-08-18 13:00:07 -0400

    partial solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import { useState } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./redux/actionCreators";
 
 function App() {
+  // const [count, setCount] = useState(0);
+  const count = useSelector((state) => state.counterReducer.count);
+  const loggedIn = useSelector((state) => state.loginReducer.status);
+  const dispatch = useDispatch();
+
+  const handleIncrementClick = () => {
+    dispatch(increment());
+  };
+
+  const handelDecrementClick = () => {
+    dispatch(decrement());
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Count: {count}</h1>
+      <div>
+        <button onClick={handleIncrementClick}>+</button>
+        <button onClick={handelDecrementClick}>-</button>
+      </div>
+      <div>
+        {loggedIn ? (
+          <button
+            onClick={() => {
+              dispatch({ type: "LOGOUT" });
+            }}
+          >
+            Log Out
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              dispatch({ type: "LOGIN" });
+            }}
+          >
+            Log In
+          </button>
+        )}
+      </div>
     </div>
   );
 }
